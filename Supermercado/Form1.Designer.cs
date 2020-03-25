24b6427710aedd13a59e90ec26bde0ba5c816536
cape cape <cape20@hotmail.es>
2020-03-25 16:26:13 -0300

    teminado

--- Supermercado/Form1.Designer.cs
+++ Supermercado/Form1.Designer.cs
@@ -28,13 +28,332 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtPeso1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblproducto1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPrecio1 = new System.Windows.Forms.TextBox();
+            this.lblproducto2 = new System.Windows.Forms.Label();
+            this.lblproducto3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtProducto1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblMuestrapreTotal = new System.Windows.Forms.Label();
+            this.label9lblMuestrapreTotalDto = new System.Windows.Forms.Label();
+            this.lblMuestrapre3 = new System.Windows.Forms.Label();
+            this.lblMuestrapre2 = new System.Windows.Forms.Label();
+            this.lblMuestrapre1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtProducto2 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtPrecio2 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtPeso2 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtProducto3 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtPrecio3 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtPeso3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtPeso1
+            // 
+            this.txtPeso1.Location = new System.Drawing.Point(302, 91);
+            this.txtPeso1.Name = "txtPeso1";
+            this.txtPeso1.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(678, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Facturar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblproducto1
+            // 
+            this.lblproducto1.AutoSize = true;
+            this.lblproducto1.Location = new System.Drawing.Point(226, 253);
+            this.lblproducto1.Name = "lblproducto1";
+            this.lblproducto1.Size = new System.Drawing.Size(0, 13);
+            this.lblproducto1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(224, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PESO (Kg)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(429, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "PRECIO X KG";
+            // 
+            // txtPrecio1
+            // 
+            this.txtPrecio1.Location = new System.Drawing.Point(528, 91);
+            this.txtPrecio1.Name = "txtPrecio1";
+            this.txtPrecio1.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio1.TabIndex = 4;
+            // 
+            // lblproducto2
+            // 
+            this.lblproducto2.AutoSize = true;
+            this.lblproducto2.Location = new System.Drawing.Point(226, 280);
+            this.lblproducto2.Name = "lblproducto2";
+            this.lblproducto2.Size = new System.Drawing.Size(0, 13);
+            this.lblproducto2.TabIndex = 6;
+            // 
+            // lblproducto3
+            // 
+            this.lblproducto3.AutoSize = true;
+            this.lblproducto3.Location = new System.Drawing.Point(226, 308);
+            this.lblproducto3.Name = "lblproducto3";
+            this.lblproducto3.Size = new System.Drawing.Size(0, 13);
+            this.lblproducto3.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(2, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "PRODUCTO";
+            // 
+            // txtProducto1
+            // 
+            this.txtProducto1.Location = new System.Drawing.Point(101, 88);
+            this.txtProducto1.Name = "txtProducto1";
+            this.txtProducto1.Size = new System.Drawing.Size(100, 20);
+            this.txtProducto1.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(237, 343);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 10;
+            // 
+            // lblMuestrapreTotal
+            // 
+            this.lblMuestrapreTotal.AutoSize = true;
+            this.lblMuestrapreTotal.Location = new System.Drawing.Point(328, 343);
+            this.lblMuestrapreTotal.Name = "lblMuestrapreTotal";
+            this.lblMuestrapreTotal.Size = new System.Drawing.Size(0, 13);
+            this.lblMuestrapreTotal.TabIndex = 11;
+            // 
+            // label9lblMuestrapreTotalDto
+            // 
+            this.label9lblMuestrapreTotalDto.AutoSize = true;
+            this.label9lblMuestrapreTotalDto.Location = new System.Drawing.Point(437, 343);
+            this.label9lblMuestrapreTotalDto.Name = "label9lblMuestrapreTotalDto";
+            this.label9lblMuestrapreTotalDto.Size = new System.Drawing.Size(0, 13);
+            this.label9lblMuestrapreTotalDto.TabIndex = 12;
+            // 
+            // lblMuestrapre3
+            // 
+            this.lblMuestrapre3.AutoSize = true;
+            this.lblMuestrapre3.Location = new System.Drawing.Point(318, 308);
+            this.lblMuestrapre3.Name = "lblMuestrapre3";
+            this.lblMuestrapre3.Size = new System.Drawing.Size(0, 13);
+            this.lblMuestrapre3.TabIndex = 15;
+            // 
+            // lblMuestrapre2
+            // 
+            this.lblMuestrapre2.AutoSize = true;
+            this.lblMuestrapre2.Location = new System.Drawing.Point(318, 280);
+            this.lblMuestrapre2.Name = "lblMuestrapre2";
+            this.lblMuestrapre2.Size = new System.Drawing.Size(0, 13);
+            this.lblMuestrapre2.TabIndex = 14;
+            // 
+            // lblMuestrapre1
+            // 
+            this.lblMuestrapre1.AutoSize = true;
+            this.lblMuestrapre1.Location = new System.Drawing.Point(318, 253);
+            this.lblMuestrapre1.Name = "lblMuestrapre1";
+            this.lblMuestrapre1.Size = new System.Drawing.Size(0, 13);
+            this.lblMuestrapre1.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(2, 125);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "PRODUCTO";
+            // 
+            // txtProducto2
+            // 
+            this.txtProducto2.Location = new System.Drawing.Point(101, 125);
+            this.txtProducto2.Name = "txtProducto2";
+            this.txtProducto2.Size = new System.Drawing.Size(100, 20);
+            this.txtProducto2.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(429, 128);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(75, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "PRECIO X KG";
+            // 
+            // txtPrecio2
+            // 
+            this.txtPrecio2.Location = new System.Drawing.Point(528, 128);
+            this.txtPrecio2.Name = "txtPrecio2";
+            this.txtPrecio2.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio2.TabIndex = 18;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(224, 128);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(58, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "PESO (Kg)";
+            // 
+            // txtPeso2
+            // 
+            this.txtPeso2.Location = new System.Drawing.Point(302, 128);
+            this.txtPeso2.Name = "txtPeso2";
+            this.txtPeso2.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso2.TabIndex = 16;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(2, 166);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(68, 13);
+            this.label11.TabIndex = 27;
+            this.label11.Text = "PRODUCTO";
+            // 
+            // txtProducto3
+            // 
+            this.txtProducto3.Location = new System.Drawing.Point(101, 166);
+            this.txtProducto3.Name = "txtProducto3";
+            this.txtProducto3.Size = new System.Drawing.Size(100, 20);
+            this.txtProducto3.TabIndex = 26;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(429, 169);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(75, 13);
+            this.label12.TabIndex = 25;
+            this.label12.Text = "PRECIO X KG";
+            // 
+            // txtPrecio3
+            // 
+            this.txtPrecio3.Location = new System.Drawing.Point(528, 169);
+            this.txtPrecio3.Name = "txtPrecio3";
+            this.txtPrecio3.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio3.TabIndex = 24;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(224, 169);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(58, 13);
+            this.label13.TabIndex = 23;
+            this.label13.Text = "PESO (Kg)";
+            // 
+            // txtPeso3
+            // 
+            this.txtPeso3.Location = new System.Drawing.Point(302, 169);
+            this.txtPeso3.Name = "txtPeso3";
+            this.txtPeso3.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso3.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtProducto3);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txtPrecio3);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.txtPeso3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtProducto2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtPrecio2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtPeso2);
+            this.Controls.Add(this.lblMuestrapre3);
+            this.Controls.Add(this.lblMuestrapre2);
+            this.Controls.Add(this.lblMuestrapre1);
+            this.Controls.Add(this.label9lblMuestrapreTotalDto);
+            this.Controls.Add(this.lblMuestrapreTotal);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtProducto1);
+            this.Controls.Add(this.lblproducto3);
+            this.Controls.Add(this.lblproducto2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPrecio1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblproducto1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPeso1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtPeso1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblproducto1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPrecio1;
+        private System.Windows.Forms.Label lblproducto2;
+        private System.Windows.Forms.Label lblproducto3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtProducto1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblMuestrapreTotal;
+        private System.Windows.Forms.Label label9lblMuestrapreTotalDto;
+        private System.Windows.Forms.Label lblMuestrapre3;
+        private System.Windows.Forms.Label lblMuestrapre2;
+        private System.Windows.Forms.Label lblMuestrapre1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtProducto2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtPrecio2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtPeso2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtProducto3;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtPrecio3;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox txtPeso3;
     }
 }
 
--- Supermercado/Form1.cs
+++ Supermercado/Form1.cs
@@ -16,5 +16,49 @@ namespace Supermercado
         {
             InitializeComponent();
         }
+
+        int kilos;
+        int precio;
+        double dto;
+        double producto1;
+       double producto2;
+        double producto3;
+        
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblproducto1.Text = txtProducto1.Text;
+            lblproducto2.Text = txtProducto2.Text;
+            lblproducto3.Text = txtProducto3.Text;
+
+            producto1 = Convert.ToDouble(txtPrecio1.Text) * Convert.ToDouble(txtPeso1.Text);
+            producto2 = Convert.ToDouble(txtPrecio2.Text) * Convert.ToDouble(txtPeso2.Text);
+            producto3 = Convert.ToDouble(txtPrecio3.Text) * Convert.ToDouble(txtPeso3.Text);
+
+            lblMuestrapre1.Text = Convert.ToString(producto1) +"  PESOS";
+            lblMuestrapre2.Text = Convert.ToString(producto2) + "  PESOS";
+            lblMuestrapre3.Text = Convert.ToString(producto3) + "  PESOS";
+            dto = producto1 + producto2 + producto3;
+            lblMuestrapreTotal.Text =Convert.ToString(dto) + "  PESOS";
+            if (dto > 100)
+            {
+                label9lblMuestrapreTotalDto.Text = Convert.ToString(dto * 90 / 100) +" Pesos 10 % de descuento por su compra superior a $ 100  ";
+
+            }
+            else 
+            {
+                label9lblMuestrapreTotalDto.Text = "para su descuento debe superar los 100 pesos";
+            }
+
+
+        }
     }
+
+
+
+
+     
+    
 }
